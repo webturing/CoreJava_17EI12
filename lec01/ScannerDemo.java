db9b19311f6e1bd89694babd3aacf05a44e37671
ZHAO Jing <ceo@webturing.com>
2019-09-06 21:41:43 +0800

    Add files via upload

---
+++ lec01/ScannerDemo.java
@@ -0,0 +1,7 @@
+
+public class ScannerDemo {
+public static void main(String[] args) {
+	int a=123,b=456;
+	System.out.println(a+b);
+}
+}
---
+++ lec01/Weclome.java
@@ -0,0 +1,5 @@
+public class Weclome {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
