f699f3e98ccd688e819978bd5da7b70529b06449
ZHAO Jing <ceo@webturing.com>
2019-09-06 21:41:21 +0800

    Add files via upload

---
+++ lec01/FormatOutput.java
@@ -0,0 +1,7 @@
+public class FormatOutput {
+	public static void main(String[] args) {
+		System.out.print("Hello world\n");
+		System.out.println("Hello world");
+		System.out.printf("%.2f",7.0/5);
+	}
+}
---
+++ lec01/LifeLength.java
@@ -0,0 +1,8 @@
+public class LifeLength {
+	public static void main(String[] args) {
+		//int max=21ƒ| 1 1.0 1.0f 1L
+		System.out.println(100 * 365 * 24 * 3600 * 1000);
+		System.out.println(100 * 365 * 24 * 3600 * 1000 * 1L);
+		System.out.println(1L*100 * 365 * 24 * 3600 * 1000 );
+	}
+}
